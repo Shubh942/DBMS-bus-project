52e95f9e4cd18635b6920b40a7a62d9d353e0c67
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2022-09-12 00:04:45 +0530

    change in scroll behaviour

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let bus=document.getElementById('upper');
         window.addEventListener('scroll',function(){
             let value=window.scrollY;
             
-            bus.style.marginLeft=value*4+'px';
+            bus.style.marginLeft=value*2.5+'px';
         });
