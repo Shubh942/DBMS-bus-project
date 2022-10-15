827b7f94d40858e281a91915f4c9aff51adc0ce7
SOHAM2312 <soham2312@SOHAM.mshome.net>
2022-10-16 01:55:56 +0530

    PLEASE MERGE IT

--- passwordverification.js
+++ passwordverification.js
@@ -1,13 +1,34 @@
 function Validate() {
-    var password = document.getElementById("txtPassword").value;
+    var name=document.getElementById("txtName").value;
+    if(name==""){
+        document.getElementById("message1").innerHTML = "Please Enter Name";
+    }
+    else{
+        document.getElementById("message1").innerHTML = "";
+    }
+    
+   
+    var roll_no=document.getElementById("txtRollNo").value;
+    if(roll_no==""){
+        document.getElementById("message2").innerHTML = "Please Enter Roll No";
+    }
+    else{
+        document.getElementById("message2").innerHTML = "";
+    }
+        var email=document.getElementById("txtEmail").value;
+        if(email==""){
+            document.getElementById("message3").innerHTML = "Please Enter Email";
+        }
+        else{
+            document.getElementById("message3").innerHTML = "";
+        }
+        var password = document.getElementById("txtPassword").value;
     var confirmPassword = document.getElementById("txtConfirmPassword").value;
-    if (password != confirmPassword) {
+    if (password != confirmPassword||password==""||confirmPassword=="") {
         document.getElementById("message").innerHTML = "Wrong Password"; 
-        return false;
     }
     else if(password == confirmPassword){
         document.getElementById("message").innerHTML = "";
         return true;
     }
-    return true;
 }
