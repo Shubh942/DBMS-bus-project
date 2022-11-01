837515b5bdfb87db045cc00ebb0536b3ce7047a2
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2022-11-01 23:48:38 +0530

    Added forget password

---
+++ public/javascripts/forgetpass.js
@@ -0,0 +1,6 @@
+const clon=document.querySelector('.clon');
+const h3=document.querySelector('.inne');
+clon.addEventListener('click',()=>{
+    document.querySelector('.inne').innerHTML="Password Sent to your given mail"
+    
+})
